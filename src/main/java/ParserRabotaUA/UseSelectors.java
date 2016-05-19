94030371a5c747b29c1ae3e49ce991461590e6df
InStaller <shchepkin.alex@gmail.com>
2016-05-19 20:26:47 +0300

    There is opportunity to search the elements by selector

--- src/main/java/ParserRabotaUA/UseSelectors.java
+++ src/main/java/ParserRabotaUA/UseSelectors.java
@@ -24,9 +24,10 @@ public class UseSelectors {
         String URL = "http://rabota.ua/jobsearch/vacancy_list?regionId=1&keyWords=";
         for (int i = 1; true; i++) {
             try {
+
 // Connect and read a page
                 doc = Jsoup.connect(URL + keyWords + "&period=2&lastdate=" + sdf.format(currentDate) + "&pg=" + i).get();
-//
+
 // Assert whether it is a last page
                 if (doc.body().select(".rua-p-t_20").text().equals("Мы не нашли ничего, что точно соответствует вашему запросу!")) {
                     System.out.println();
@@ -39,7 +40,7 @@ public class UseSelectors {
                 for (int j = 0; j < doc.body().select(".t").size(); j++) {
                     System.out.println(doc.body().select(".t").get(j).text()); //name of vacancy
                     System.out.println(doc.body().select(".s").get(j).text()); //name of company
-                    if(j<doc.body().select(".t").size()-1)System.out.println(doc.body().select(".dt").get(j).text()); //time of added
+//                    if(j<doc.body().select(".t").size()-1)System.out.println(doc.body().select(".dt").get(j).text()); //time of added
                     System.out.println("http://rabota.ua" + doc.body().select(".t").get(j).attr("href")); //link
                     System.out.println();
                 }
